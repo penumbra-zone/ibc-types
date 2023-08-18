2d21eeb71f9eb3b1ad40336aa9c06f0d5b3d9b6c
Erwan <erwanor@penumbralabs.xyz>
2023-08-17 22:18:58 -0400

    ci(release): add a dry-run phase

--- release.sh
+++ release.sh
@@ -62,6 +62,12 @@ publish() {
   echo ""
 }
 
+dry_publish() {
+  echo "Publishing crate $1..."
+  cargo publish --dry-run --manifest-path "$(get_manifest_path "${1}")" ${CARGO_PUBLISH_FLAGS}
+  echo ""
+}
+
 wait_until_available() {
   echo "Waiting for crate ${1} to become available via crates.io..."
   for retry in {1..5}; do
@@ -82,6 +88,38 @@ wait_until_available() {
   echo "Waiting an additional 10 seconds for crate to propagate through CDN..."
   sleep 10
 }
+
+echo "Doing a dry-run"
+for crate in ${CRATES}; do
+  dry_publish "${crate}"
+done
+
+echo "Attempting to publish crate(s): ${CRATES}"
+
+# Since crates.io has rate-limit and we always want to publish 
+# every crate in lockstep, we need to wait 5min every 5 pubs
+COUNTER=1
+
+for crate in ${CRATES}; do
+  VERSION="$(get_local_version "${crate}")"
+  ONLINE_DATE="$(check_version_online "${crate}" "${VERSION}")"
+  echo "${crate} version number: ${VERSION}"
+  if [ -n "${ONLINE_DATE}" ]; then
+    echo "${crate} ${VERSION} has already been published at ${ONLINE_DATE}, skipping"
+    continue
+  fi
+
+  if [ $COUNTER -eq 5 ]; then
+    echo "Reached 5 crates, waiting for 5 minutes..."
+    sleep 300 # Wait for 5 minutes
+    COUNTER=1 # Reset the counter
+  fi
+
+  publish "${crate}"
+  wait_until_available "${crate}" "${VERSION}"
+  ((COUNTER++))
+done
+
 
 echo "Attempting to publish crate(s): ${CRATES}"
 
