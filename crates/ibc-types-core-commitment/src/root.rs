f209ef484748df57dfafdfc053c2e2d3ba6250c1
Erwan Or <erwan.ounn.84@gmail.com>
2023-09-18 17:40:58 -0400

    ibc-types: prepare release `v0.6.1` (#53)
    * ibc-core-commitment: debug impl with hex encoded `MerkleRoot`
    
    * ibc-types-core-commitment: utf8 debug for `Packet`
    
    * ibc-types: use `v0.6.1`
    
    * ibc-types: adjust internal dep graph

--- crates/ibc-types-core-commitment/src/root.rs
+++ crates/ibc-types-core-commitment/src/root.rs
@@ -1,11 +1,19 @@
+use core::fmt::Debug;
+
 use crate::prelude::*;
 
 use ibc_proto::ibc::core::commitment::v1::MerkleRoot as RawMerkleRoot;
 use ibc_types_domain_type::{DomainType, TypeUrl};
 
-#[derive(Clone, Debug, PartialEq, Eq)]
+#[derive(Clone, PartialEq, Eq)]
 pub struct MerkleRoot {
     pub hash: Vec<u8>,
+}
+
+impl Debug for MerkleRoot {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        write!(f, "{:?}", hex::encode(&self.hash))
+    }
 }
 
 impl TypeUrl for MerkleRoot {
