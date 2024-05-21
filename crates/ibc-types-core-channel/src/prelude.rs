a2148a6aa231e09f67c6ea1bc79ef1bbd2f7cdad
katelyn martin <me+cratelyn@katelyn.world>
2024-05-21 18:04:13 -0400

    core-channel: 🎧 prelude allows "unused" imports (#87)

--- crates/ibc-types-core-channel/src/prelude.rs
+++ crates/ibc-types-core-channel/src/prelude.rs
@@ -1,5 +1,7 @@
 pub use core::prelude::v1::*;
 
+// allow `unused_imports`, rustc errantly claims this `vec!` is not used.
+#[allow(unused_imports)]
 // Re-export according to alloc::prelude::v1 because it is not yet stabilized
 // https://doc.rust-lang.org/src/alloc/prelude/v1.rs.html
 pub use alloc::{
