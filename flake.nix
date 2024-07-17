4cd8fc0ca18581958d9c824cfee8cefe2cb9b0d4
Chris Czub <chris@penumbralabs.xyz>
2024-07-17 14:23:19 -0400

    Bump ics23 dep to 0.11.3, bump ibc-types version to 0.14.1 (#91)

--- flake.nix
+++ flake.nix
@@ -33,7 +33,7 @@
         let
           ibc-types = (craneLib.buildPackage {
             pname = "ibc-types";
-            version = "0.14.0";
+            version = "0.14.1";
             nativeBuildInputs = [ pkg-config ];
             buildInputs = [ openssl ];
             inherit src system PKG_CONFIG_PATH;
